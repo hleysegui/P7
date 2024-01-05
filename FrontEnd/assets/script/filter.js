438897d4d2f75f55547af6035e42e02f0c252adf
ashley segui <ashley.segui@icloud.com>
2024-01-05 17:43:28 +0100

    fix filter by category

--- FrontEnd/assets/script/filter.js
+++ FrontEnd/assets/script/filter.js
@@ -1,7 +1,7 @@
 const url = "http://localhost:5678/api/categories"
 const divFitler = document.querySelector('.filter')
 
-async function getCategories() {
+export async function getCategories() {
     try {
         const response = await fetch(url)
         const categories = await response.json()
@@ -17,10 +17,8 @@ listCategory.then((categories) => showCategories(categories))
 
 function showCategories(categories) {
 
-    let btn = document.createElement('button')
-    btn.textContent = "Tous"
-    btn.classList.add("input-cat")
-    divFitler.appendChild(btn)
+    let all = {id:0, name:"Tous"}
+    categories.unshift(all)
 
     for (let index = 0; index < categories.length; index++) {
         let cat = categories[index]
@@ -30,40 +28,46 @@ function showCategories(categories) {
         inputFilter.dataset.id = cat.id
         inputFilter.textContent = cat.name
         divFitler.appendChild(inputFilter)
+
+        if(cat.id === 0) {
+            inputFilter.classList.add("active")
+        }
     }
 
     const listCategory = document.querySelectorAll('.input-cat')
     const galleryItem = document.querySelectorAll('.gallery figure')
 
-    for (let t = 0; t < listCategory.length; t++)
-        listCategory[t].addEventListener("click", function () {
-            filterByCategory(listCategory[t], galleryItem),
+    for (let t = 0; t < listCategory.length; t++) {
+        listCategory[t].addEventListener("click", function (event) {
+            filterByCategory(listCategory, event),
                 toggleGallery(listCategory[t].dataset.id, galleryItem)
-
         });
+
+    }
 }
 
-// Add remove class active 
-function filterByCategory(t, galleryItem) {
 
-    galleryItem.forEach(t => {
-        t.classList.remove("active")
+// Add remove class active 
+function filterByCategory(listCategory, event) {
+
+    listCategory.forEach(element => {
+        element.classList.remove("active")
     });
-    t.classList.add("active")
+    event.target.classList.add("active")
 }
 
 //hidden or display item
 function toggleGallery(t, galleryItem) {
 
-    if(t === "Tous" || t === undefined) {
+     if(t == 0) {
         galleryItem.forEach(item => {
             item.style.display = "block"
-        });
+        })
     } else {
         galleryItem.forEach(item => {
             item.dataset.id === t  
                 ? (item.style.display = "block")
                 : (item.style.display = "none")
-        });
-    }
+        })
+    } 
 }
